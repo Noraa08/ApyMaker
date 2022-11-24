b46ecf550e13da842e9d78cfef65b345c0066e42
yume <yumeee.lol@gmail.com>
2022-11-24 23:00:29 +1100

    add ui components

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { store } from "../src/pages/ReduxUseCase/store";
 import { Provider } from "react-redux";
+import "./index.css";
 
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import HooksUseCase from "./pages/HooksUseCase";
 import ReduxUseCase from "./pages/ReduxUseCase";
-import "./index.css";
+import Counter from "./pages/ReduxUseCase/Counter";
+
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -21,7 +23,12 @@ root.render(
           <Route path="/" element={<Home />} />
           <Route path="/login-example" element={<Login />} />
           <Route path="/hooks-use-case" element={<HooksUseCase />} />
-          <Route path="/redux-use-case" element={<ReduxUseCase />} />
+
+          <Route path="/redux-use-case" >
+            <Route index element={<ReduxUseCase />} />
+            <Route path="/redux-use-case/counter" element={<Counter />} />
+          </Route>
+
         </Routes>
       </Router>
     </Provider>
--- src/pages/ReduxUseCase/index.tsx
+++ src/pages/ReduxUseCase/index.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import Button from '../../ui/atoms/Button'
 import Counter from './Counter'
 
 const ReduxUseCase = () => {
+  const navigate = useNavigate()
+
   return (
+    <>
+    <Button theme={'outlined'} text="Counter" onClick={()=> navigate('/redux-use-case/counter')}/>
     <Counter />
+    </>
   )
 }
 
